95ce1db2fb3f811c391adeb0b361e3795b1e40ab
levLezhnin <lezhnin.lev78@gmail.com>
2025-11-22 15:06:33 +0500

    add user related models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class UserRoles:
+    ROLE_CHOICES = [
+        'User',
+        'Admin',
+        'Staff'
+    ]
+
+
+class User(models.Model):
+    """
+    Пользователь
+    """
+    name = models.TextField(null=False, blank=False)
+    lastname = models.TextField(null=False, blank=False)
+    patronymic = models.TextField(null=True, blank=True) # опционально
+    role = models.TextField(null=False, blank=False, choices=UserRoles.ROLE_CHOICES)
+    email = models.EmailField(null=True, blank=True) # опционально
+    phone = models.TextField(null=True, blank=True, max_length=20) # опционально
