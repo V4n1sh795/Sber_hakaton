5c99ce6b964e8911af564c79138b7ab30bd1f1a7
Иванов Владислав <ivanov.vlad135@gmail.com>
2025-11-22 17:55:14 +0500

    update event core

--- events/models.py
+++ events/models.py
@@ -4,7 +4,7 @@ class Event(models.Model):
     """
     События
     """
-    name = models.TextField(max_length=255, null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False)
     date = models.DateField(null=False, blank=False)
     description = models.TextField(null=True, blank=True)
     rating = models.DecimalField(null=False, blank=False, max_digits=3, decimal_places=2, default=0.0)
--- events/views.py
+++ events/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from .models import Event
 
-# Create your views here.
+
+def event_list(request):
+    """
+    Список событий с пагинацией
+    """
+    events = Event.objects.all().order_by('-date')
+    
+    # Пагинация - 10 событий на страницу
+    paginator = Paginator(events, 10)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+    
+    context = {
+        'page_obj': page_obj,
+        'events': page_obj.object_list,
+    }
+    
+    return render(request, 'events/event_list.html', context)
+
+
+def event_detail(request, event_id):
+    """
+    Детальная страница события
+    """
+    event = get_object_or_404(Event, pk=event_id)
+    
+    context = {
+        'event': event,
+    }
+    
+    return render(request, 'events/event_detail.html', context)
