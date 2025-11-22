8dbc5c998e0c3b12bfcfd2d1d1b6accfc08525be
levLezhnin <lezhnin.lev78@gmail.com>
2025-11-22 23:17:32 +0500

    books changes - book copy model added - added views for list books and get full book info - added templates

--- books/models.py
+++ books/models.py
@@ -8,6 +8,7 @@ class Book(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
     author = models.CharField(max_length=255, null=False, blank=False)
     genre = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
     cover_photo = models.ImageField(
                             upload_to='covers/full/',
                             null=True, 
@@ -22,3 +23,18 @@ class Book(models.Model):
                             verbose_name='Превью обложки',
                             help_text='Генерируемая превью книги'
     )
+
+class BookCopy(models.Model):
+    """
+    Экземпляр книги
+    """
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book_condition_photo = models.ImageField(
+                            upload_to='copies/condition/full/',
+                            null=True, 
+                            blank=True,
+                            verbose_name='Фотография состояния книги',
+                            help_text='Загрузите фотографию исходного состояния книги'
+                        )
+    registered_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
