5b9df9b885afe474eeec1ace534fc1101e2bc92f
levLezhnin <lezhnin.lev78@gmail.com>
2025-11-23 03:38:41 +0500

    add create book form, create book copy form

--- books/models.py
+++ books/models.py
@@ -1,4 +1,8 @@
+from io import BytesIO
+import os
 from django.db import models
+from PIL import Image
+from django.core.files.base import ContentFile
 
 
 class Book(models.Model):
@@ -24,6 +28,45 @@ class Book(models.Model):
                             help_text='Генерируемая превью книги'
     )
 
+    def create_thumbnail(self):
+        """Создает thumbnail из основного изображения"""
+        try:
+            # Открываем основное изображение
+            img = Image.open(self.cover_photo.path)
+            
+            # Определяем размеры для thumbnail
+            thumbnail_size = (200, 300)
+            
+            # Конвертируем в RGB если нужно
+            if img.mode in ('RGBA', 'LA', 'P'):
+                img = img.convert('RGB')
+            
+            # Создаем thumbnail
+            img.thumbnail(thumbnail_size, Image.Resampling.LANCZOS)
+            
+            # Сохраняем в память
+            thumb_io = BytesIO()
+            img.save(thumb_io, format='JPEG', quality=80)
+            thumb_io.seek(0)
+            
+            # Формируем имя файла
+            filename = os.path.basename(self.cover_photo.name)
+            name, ext = os.path.splitext(filename)
+            thumb_filename = f"{name}_thumb.jpg"
+            
+            # Сохраняем thumbnail
+            self.cover_thumbnail.save(
+                thumb_filename,
+                ContentFile(thumb_io.getvalue()),
+                save=False
+            )
+            
+            # Сохраняем модель
+            super().save()
+            
+        except Exception as e:
+            print(f"Ошибка при создании thumbnail: {e}")
+
 class BookCopy(models.Model):
     """
     Экземпляр книги
@@ -36,5 +79,5 @@ class BookCopy(models.Model):
                             verbose_name='Фотография состояния книги',
                             help_text='Загрузите фотографию исходного состояния книги'
                         )
-    registered_at = models.DateTimeField(auto_created=True)
+    registered_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
--- books/views.py
+++ books/views.py
@@ -1,9 +1,11 @@
+from django.http import JsonResponse
 from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from django.contrib import messages
 from django.urls import reverse_lazy
+from django.db.models import Q
 
 from books.models import Book, BookCopy
-from books.forms import CreateBookForm
+from books.forms import CreateBookForm, BookFilterListForm
 from books.forms import CreateBookCopyForm, UpdateBookCopyForm
 
 # Запросы для книг
@@ -12,16 +14,18 @@ class CreateBookView(CreateView):
     model = Book
     form_class = CreateBookForm
     template_name = "books/create_form.html"
-    success_url = reverse_lazy('list')
+    success_url = reverse_lazy('books:BookPagedView')
 
     def form_valid(self, form):
+        # TODO: file size validation
+        # TODO: create thumbnail
         response = super().form_valid(form)
         messages.success(self.request, f'Книга "{self.object.title}" успешно создана!')
         return response
 
 # сервисный метод
 def get_book_entity_by_id(book_id: int) -> Book:
-    return Book.objects.filter(id = book_id)
+    return Book.objects.get(id=book_id)
 
 class BookInfoView(DetailView):
     model = Book
@@ -46,6 +50,40 @@ class BookPagedView(ListView):
     context_object_name = 'books'
     paginate_by = 30
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        
+        title = self.request.GET.get('title')
+        genre = self.request.GET.get('genre')
+        author = self.request.GET.get('author')
+        
+        # Применяем фильтры
+        if title:
+            # Поиск по частичному совпадению в названии
+            queryset = queryset.filter(title__icontains=title)
+        if genre:
+            queryset = queryset.filter(genre=genre)
+        if author:
+            queryset = queryset.filter(author=author)
+            
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Добавляем форму фильтрации в контекст
+        filter_form = BookFilterListForm(self.request.GET)
+        context['filter_form'] = filter_form
+        
+        # Добавляем информацию о текущих фильтрах
+        context['active_filters'] = {
+            'title': self.request.GET.get('title'),
+            'genre': self.request.GET.get('genre'),
+            'author': self.request.GET.get('author'),
+        }
+        
+        return context
+
 # Запросы для экземпляров книг
 
 class CreateBookCopyView(CreateView):
@@ -53,30 +91,72 @@ class CreateBookCopyView(CreateView):
     form_class = CreateBookCopyForm
     template_name = "bookcopies/create_form.html"
 
-    def form_valid(self, form):
-        response = super().form_valid(form)
+    def get_success_url(self):
         messages.success(self.request, 'Экземпляр книги успешно создан!')
+        # Убеждаемся, что объект существует и у него есть книга
+        if hasattr(self, 'object') and self.object and self.object.book:
+            return reverse_lazy('books:BookFullInfoView', kwargs={'pk': self.object.book.pk})
+        else:
+            # Fallback на список книг, если что-то пошло не так
+            return reverse_lazy('books:BookPagedView')
+    
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        book_id = self.kwargs.get('book_id')
+        print('get_form_kwargs:', book_id)
+        if book_id:
+            kwargs['initial'] = {'book': book_id}
+            kwargs['book_id'] = book_id
+        return kwargs
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        book_id = self.kwargs.get('book_id')
+        
+        if book_id:
+            book = get_book_entity_by_id(book_id)  # Используем get_object_or_404 вместо вашей функции
+            context['selected_book'] = book
+        
+        print('get selected book:', context['selected_book'])
+
+        context['page_title'] = 'Добавить экземпляр книги'
+        
+        return context
+    
+    def form_valid(self, form):
+        book_id = self.kwargs.get('book_id')
+        if book_id:
+            book = get_book_entity_by_id(book_id)
+            form.instance.book = book
+        
+        print('form valid:', form.instance.book)
+
+        # Проверяем, что книга установлена
+        if not form.instance.book:
+            form.add_error('book', 'Необходимо выбрать книгу')
+            return self.form_invalid(form)
+        
+        response = super().form_valid(form)
         return response
 
 class UpdateBookCopyView(UpdateView):
     model = BookCopy
     form_class = UpdateBookCopyForm
     template_name = "bookcopies/update_form.html"
-    success_message_template = 'Экземпляр книги успешно обновлён!'
-
-    def get_success_url(self):
-        messages.success(self.request, 'Экземпляр книги успешно обновлён!')
-        return reverse_lazy('book_copy_info', kwargs={'pk': self.object.pk})
+    context_object_name = 'book_copy'
     
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        # Для UpdateView нужно передать instance
-        if self.request.method in ('POST', 'PUT'):
-            kwargs.update({
-                'data': self.request.POST,
-                'files': self.request.FILES,
-            })
-        return kwargs
+    def get_success_url(self):
+        messages.success(self.request, 'Фото состояния успешно обновлено!')
+        return reverse_lazy('books:BookFullInfoView', kwargs={'pk': self.object.book.pk})
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+    def form_valid(self, form):
+        # Можно добавить дополнительную логику перед сохранением
+        response = super().form_valid(form)
+        return response
 
 class BookCopyInfo(DetailView):
     model = BookCopy
